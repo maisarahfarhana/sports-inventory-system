953090e1139497342d3d2c02a891d2cad5e226d8
maisarahfarhana <maisarahfarhana8@gmail.com>
2026-09-02 12:02:21 +0800

    Ensure sqflite_sw.js is included in web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "202b4f169ef5ad32b2b9c703b23ffd81",
+const RESOURCES = {"flutter_bootstrap.js": "25fa2ef8bd528e5d5879a881b6160975",
 "version.json": "01286df96407fd8681fdb414efe7f2a8",
 "index.html": "9bd0664eed06723c045c7a3ea4792a18",
 "/": "9bd0664eed06723c045c7a3ea4792a18",
