b1016893c2a64f5f03d2db3940dc106d7196c997
maisarahfarhana <maisarahfarhana8@gmail.com>
2026-09-02 11:58:14 +0800

    Add sqflite_sw.js worker for web database support

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "221f4f059d948db4c31dd15f1ba2fdd1",
+const RESOURCES = {"flutter_bootstrap.js": "202b4f169ef5ad32b2b9c703b23ffd81",
 "version.json": "01286df96407fd8681fdb414efe7f2a8",
 "index.html": "9bd0664eed06723c045c7a3ea4792a18",
 "/": "9bd0664eed06723c045c7a3ea4792a18",
